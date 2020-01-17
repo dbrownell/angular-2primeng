063f4db549406b8c1320633ce35372cb3e7a99b7
dbrownell <danbrownell@comcast.net>
2020-01-17 16:18:19 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import {ToggleButtonModule} from 'primeng/togglebutton';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, ToggleButtonModule ],
+  declarations: [ AppComponent, ButtonsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
